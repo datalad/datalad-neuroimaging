c36559ec1b23e1e4651728a352b257f13a0ae1db
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2018-03-29 17:06:03 +0200

    Adapt setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ from setuptools import setup
 
 setup(
     # basic project properties can be set arbitrarily
-    name="dmhelloworld",
+    name="datalad-medicalimaging",
     author="The DataLad Team and Contributors",
     author_email="team@datalad.org",
     version='0.1',
-    description="demo DataLad module package",
+    description="DataLad extension package for medical imaging datasets",
     # datalad command suite specs from here
     install_requires=[
         # in general datalad will be a requirement, unless the datalad extension
         # aspect is an optional component of a larger project
         'datalad',
+        'datalad-webapp',
     ],
     entry_points = {
         # 'datalad.modules' is THE entrypoint inspected by the datalad API builders
@@ -22,6 +23,9 @@ setup(
             # the entrypoint can point to any symbol of any name, as long it is
             # valid datalad interface specification (see demo in this module)
             'hello=dmhelloworld:module_suite',
-        ]
+        ],
+        'datalad.webapps': [
+            'pork=webapp.app:Pork',
+        ],
     },
 )
