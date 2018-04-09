80a86d3fb6ce54328dd05a258f583b3650067edb
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2018-04-09 16:56:04 +0200

    BF: default rules can deal with missing fields in DICOM metadata

--- datalad_neuroimaging/commands/dicom2bids_rules.py
+++ datalad_neuroimaging/commands/dicom2bids_rules.py
@@ -75,21 +75,22 @@ class DefaultRules(object):
 
     def _rules(self, record):
 
-        if record['ProtocolName'] in self.runs:
-            self.runs[record['ProtocolName']] += 1
+        protocol_name = record.get('ProtocolName', None)
+        if protocol_name in self.runs:
+            self.runs[protocol_name] += 1
         else:
-            self.runs[record['ProtocolName']] = 1
+            self.runs[protocol_name] = 1
 
         return {
                 'description': record['SeriesDescription'],
                 'comment': '',
-                'subject': apply_bids_label_restrictions(record['PatientName']),
-                'session': apply_bids_label_restrictions(record['ProtocolName']),
-                'task': apply_bids_label_restrictions(record['ProtocolName']),
-                'run': self.runs[record['ProtocolName']],
+                'subject': apply_bids_label_restrictions(record.get('PatientName', None)),
+                'session': apply_bids_label_restrictions(protocol_name),
+                'task': apply_bids_label_restrictions(protocol_name),
+                'run': self.runs[protocol_name],
                 # TODO: BIDS-conform modality; (func, anat, dwi, fmap):
                 'modality': '',
-                'id': record['SeriesNumber'],
+                'id': record.get('SeriesNumber', None),
                 }
 
 
@@ -99,6 +100,10 @@ def apply_bids_label_restrictions(value):
     """
     # only alphanumeric allowed
     # => remove everthing else
+
+    from six import string_types
+    if not isinstance(value, string_types):
+        value = str(value)
 
     import re
     pattern = re.compile('[\W_]+')  # check
