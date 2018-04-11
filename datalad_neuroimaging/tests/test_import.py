2d665273ed82c81e59617b84e0b019c266646839
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2018-04-11 15:24:49 +0200

    import doesn't work for direct mode ATM

--- datalad_neuroimaging/tests/test_import.py
+++ datalad_neuroimaging/tests/test_import.py
@@ -6,11 +6,17 @@ from datalad.api import ni_create_study
 
 from datalad.tests.utils import assert_result_count
 from datalad.tests.utils import ok_clean_git, ok_exists, ok_file_under_git
-from datalad.tests.utils import with_tempfile
+from datalad.tests.utils import with_tempfile, known_failure_direct_mode
 
 from datalad_neuroimaging.tests.utils import get_dicom_dataset, create_dicom_tarball
 
 
+# Note: In direct mode the entire three branch approach on importing seems to
+# have an issue. The tarball is a broken in symlink in 'incoming-processed',
+# before add_archive_content is even called. On the other hand annex-get works
+# for it, so it's not like nothing worked at all.
+# May be have to somehow account for direct mode when switching branches.
+@known_failure_direct_mode
 @with_tempfile
 @with_tempfile
 def test_import_tarball(filename, ds_path):
@@ -27,7 +33,6 @@ def test_import_tarball(filename, ds_path):
     assert opj(ds.path, 'user_defined_session', 'dicoms') in [s.path for s in subs]
     ok_exists(opj(ds.path, 'user_defined_session', 'studyspec.json'))
     ok_exists(opj(ds.path, 'user_defined_session', 'dicoms', 'structural'))
-
 
     # now import again, but let the import routine figure out a session name
     # based on DICOM metadata (ATM just the first occurring PatientID, I think)
