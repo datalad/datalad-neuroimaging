a83a41f12e38c3730c69cd2c956d3ad632092a4f
Michael Hanke <michael.hanke@gmail.com>
2019-04-28 16:17:04 +0200

    RF: Rename procedure to just cfg_bids

--- datalad_neuroimaging/tests/test_procedure.py
+++ datalad_neuroimaging/tests/test_procedure.py
@@ -10,7 +10,7 @@ import datalad.interface.run_procedure
 @with_tempfile
 def test_bids_procedure(path):
     ds = Dataset(path).rev_create()
-    ds.run_procedure(['cfg_bids_dataset'])
+    ds.run_procedure(['cfg_bids'])
     ds.config.reload()
     eq_('nothing',
         ds.repo.get_gitattributes(
@@ -19,7 +19,7 @@ def test_bids_procedure(path):
         set(ds.config['datalad.metadata.nativetype']))
     # check that it does nothing on a second run
     origsha = ds.repo.get_hexsha()
-    ds.run_procedure(['cfg_bids_dataset'])
+    ds.run_procedure(['cfg_bids'])
     eq_(origsha, ds.repo.get_hexsha())
     assert_repo_status(ds.path)
 
