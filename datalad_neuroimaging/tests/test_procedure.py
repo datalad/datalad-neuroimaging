c1a499783cf596e3444062e91bb93f7050a2d976
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2019-05-08 17:04:34 +0200

    BF: Don't use datalad RC functions

--- datalad_neuroimaging/tests/test_procedure.py
+++ datalad_neuroimaging/tests/test_procedure.py
@@ -1,7 +1,7 @@
 from datalad.tests.utils import (
     with_tempfile,
     eq_,
-    assert_repo_status,
+    ok_clean_git,
 )
 from datalad.distribution.dataset import Dataset
 import datalad.interface.run_procedure
@@ -9,7 +9,7 @@ import datalad.interface.run_procedure
 
 @with_tempfile
 def test_bids_procedure(path):
-    ds = Dataset(path).rev_create()
+    ds = Dataset(path).create()
     ds.run_procedure(['cfg_bids'])
     ds.config.reload()
     eq_('nothing',
@@ -21,5 +21,5 @@ def test_bids_procedure(path):
     origsha = ds.repo.get_hexsha()
     ds.run_procedure(['cfg_bids'])
     eq_(origsha, ds.repo.get_hexsha())
-    assert_repo_status(ds.path)
+    ok_clean_git(ds.path)
 
